0f85cc607f7ba53d7ad2fc909cbc1da6e30401bc
Jurody <kamilov.2000@gmail.com>
2021-02-10 13:30:29 +0500

    ddd

--- flasktest.py
+++ flasktest.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 # flask\env\Scripts\activate
+
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask.db'
 app.config['SOLALCHEMY_TRACK_MODIFICATIONS'] = False
